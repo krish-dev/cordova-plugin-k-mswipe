ef750173e4bf3587f366e015d09c8af70332d848
Krishnendu Sekhar Das <krishnendu.sekhardas@indusnet.co.in>
2018-07-05 12:03:32 +0530

    - type definition change

---
+++ types/interfaces.d.ts
@@ -0,0 +1,86 @@
+export interface Mswipe {
+    pay(
+        paymentInfo: PaymentInfo,
+        callbackSuccess: (res: any) => void,
+        callbackError: (err: any) => void): void;
+
+    disconnect(
+        callbackSuccess: (res: any) => void,
+        callbackError: (err: any) => void): void;
+
+    verifyMarchent(
+        credential: MarchentLogin,
+        callbackSuccess: (res: any) => void,
+        callbackError: (err: any) => void): void;
+}
+
+export interface MarchentLogin {
+    userId: string;
+    pin: string;
+}
+
+export interface PaymentInfo {
+    amount: string;
+    mobileNumber: string;
+    invoiceNumber: string;
+    email?: string;
+    notes?: string;
+    extraNote1?: string;
+    extraNote2?: string;
+    extraNote3?: string;
+    extraNote4?: string;
+    extraNote5?: string;
+    extraNote6?: string;
+    extraNote7?: string;
+    extraNote8?: string;
+    extraNote9?: string;
+    extraNote10?: string;
+}
+
+export interface TransactionInfo {
+    bankName: string;
+    merchantName: string;
+    merchantAdd: string;
+    dateTime: string;
+    mId: string;
+    tId: string;
+    batchNo: string;
+    voucherNo: string;
+    refNo: string;
+    saleType: string;
+    cardNo: string;
+    cardLast4Digits: string;
+    trxType: string;
+    cardType: string;
+    expDate: string;
+    emvSigExpDate: string;
+    cardHolderName: string;
+    currency: string;
+    cashAmount: string;
+    baseAmount: string;
+    tipAmount: string;
+    totalAmount: string;
+    authCode: string;
+    rrNo: string;
+    certif: string;
+    appId: string;
+    appName: string;
+    tvr: string;
+    tsi: string;
+    appVersion: string;
+    isPinVarifed: string;
+    stan: string;
+    cardIssuer: string;
+    emiPerMonthAmount: string;
+    total_Pay_Amount: string;
+    noOfEmi: string;
+    interestRate: string;
+    billNo: string;
+    firstDigitsOfCard: string;
+    isConvenceFeeEnable: string;
+    invoiceNo: string;
+    trxDate: string;
+    trxImgDate: string;
+    chequeDate: string;
+    chequeNo: string;
+}
